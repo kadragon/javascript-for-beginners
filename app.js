0e22c67333aa8a3c8c94a9f6d29fdf9ff279d82a
kangdonguk <kangdongouk@gmail.com>
2022-09-28 17:58:00 +0900

    #2.3

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const a = 5;
 const b = 2;
 // camelCase
-const myName = "nico";
+let myName = "nico";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("Hello " + myName);
+
+myName = "nicolas";
+
+console.log("Your new name is " + myName);
