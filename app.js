83e4551b35dedab43aeeda2401b00c89244ae1bf
kangdonguk <kangdongouk@gmail.com>
2022-09-30 11:01:22 +0900

    #3.0

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-const age = parseInt(prompt("How old are you?"));
+console.log(document.title);
 
-if (isNaN(age) || age < 0) {
-  console.log("Please write a real positive number.");
-} else if (age < 18) {
-  console.log("You are too yonng.");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink.");
-} else if (age <= 80) {
-  console.log("You should exercise.");
-} else {
-  console.log("You can do whatever you want.");
-}
+document.title = "Momentum App";
